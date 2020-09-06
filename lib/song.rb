0b1e9acb051f33aec9280e247402666045d03aea
Javier Aguilar <coyote176@msn.com>
2020-09-06 06:24:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@all= []
   
    def initialize(name,artist=nil,genre=nil)
-   # @name = name
+    @name = name
     # @artist = artist
     self.artist=(artist) if !artist.nil?
     # @genre = genre
