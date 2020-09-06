9ca57f2c4abbe18a5fecebe4fe7607321e41b0fd
Javier Aguilar <coyote176@msn.com>
2020-09-06 09:05:37 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,6 +36,121 @@ class MusicLibraryController
     end
   end
   
-  
+ def list_songs
+    @song_array = []
+    abc_songs = Song.all.sort_by{ |song| song.name}
+
+    counter = 1
+    abc_songs.collect do |song|
+    @song_array << "#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      counter += 1
+    end
+
+    @song_array.each do |song|
+      puts "#{song}"
+    end
+  end
+
+  def list_artists
+    artist_array =[]
+
+    Artist.all.each do |artist|
+      artist_array << artist.name
+    end
+
+    abc_artists = artist_array.sort
+
+    counter = 1
+    abc_artists.each do |artist|
+      puts "#{counter}. #{artist}"
+      counter += 1
+    end
+  end
+
+  def list_genres
+   genre_array =[]
+
+    Genre.all.each do |genre|
+      genre_array << genre.name
+    end
+
+    abc_genres = genre_array.sort
+
+    counter = 1
+      abc_genres.each do |genre|
+        puts "#{counter}. #{genre}"
+        counter += 1
+      end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    song_array = []
+    artist = Artist.find_by_name(input)
+
+    if artist != nil
+      abc_songs = artist.songs.sort_by{ |song| song.name}
+
+      abc_songs.collect do |song|
+        song_array << "#{song.name} - #{song.genre.name}"
+      end
+
+      counter = 1
+      song_array.each do |song|
+        puts "#{counter}. #{song}"
+        counter += 1
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.chomp
+    song_array = []
+    genre = Genre.find_by_name(input)
+
+    if genre != nil
+
+      abc_songs = genre.songs.sort_by{ |song| song.name}
+
+      abc_songs.collect do |song|
+        song_array << "#{song.artist.name} - #{song.name}"
+      end
+
+      counter = 1
+      song_array.each do |song|
+        puts "#{counter}. #{song}"
+        counter += 1
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.chomp
+
+    song_array = []
+
+    abc_songs = Song.all.sort_by{ |song| song.name}
+
+    counter = 1
+    abc_songs.collect do |song|
+      song_array << "#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      counter += 1
+    end
+
+    song_array.each do |song|
+      song_parts = song.split(".")
+      song_number = song_parts[0]
+
+      if song_number == input
+       song_info = song_parts[1].split(" -")
+       song_artist = song_info[0]
+       song_name = song_info[1]
+       puts "Playing#{song_name} by#{song_artist}"
+      end
+    end
+  end
 
 end
